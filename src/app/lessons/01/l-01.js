5f167eb0e621bc5af8aa4345b6acf80cab4bc1a6
Alexander Vakula <alexander.vakula@synapse.com>
2018-05-04 10:50:37 +0300

    Homework

--- src/app/lessons/01/l-01.js
+++ src/app/lessons/01/l-01.js
@@ -399,9 +399,8 @@ console.log('\nLesson 01 - Homework:\n\n!!! Please answer the questions in this 
 // Task 01.05 - 3 points
 // Q: Please write a function which calculates the difference between two numbers
 // A: var a, b, diff;
-function firstOne (a, b) {
-    diff =  b - a;	
-    return diff
+function substruct (a, b) {
+    return b - a
 };
-console.log(firstOne (120, 326));
+console.log(substruct (120, 326));
 // Lesson 01 Homework - End
